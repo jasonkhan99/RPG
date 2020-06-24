7612768fd33f1985e9c0c77d0cdf646af1ca476e
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-24 11:04:11 -0700

    red green refactor test for success
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- __tests__/RPG.tests.js
+++ __tests__/RPG.tests.js
@@ -4,11 +4,11 @@ import { CharacterCreator } from './../src/RPG.js';
 describe('CharacterCreator', () => {
 
   test('should create a character with user selected input', () => {
-    const character = new CharacterCreator("Jason", "hacker", [], [], 0);
+    const character = new CharacterCreator("Jason", "hacker");
     expect(character.name).toEqual("Jason");
     expect(character.profile).toEqual("hacker");
     expect(character.inventory).toEqual([]);
     expect(character.stats).toEqual([]);
-    expect(character.experience).toEqual(10);
+    expect(character.experience).toEqual(0);
   });
 });

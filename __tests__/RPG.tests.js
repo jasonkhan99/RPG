7d3d5a2d2f1e8bd53edb0dc7784242777edc4a3c
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-24 13:51:56 -0700

    test add statlist with hacker
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- __tests__/RPG.tests.js
+++ __tests__/RPG.tests.js
@@ -2,24 +2,45 @@ import { CharacterCreator } from './../src/RPG.js';
 import { StatList } from './../src/RPG.js';
 
 describe('CharacterCreator', () => {
+  const character = new CharacterCreator("Jason", "Hacker");
 
   test('should create a character with user selected input', () => {
-    const character = new CharacterCreator("Jason", "hacker");
     expect(character.name).toEqual("Jason");
-    expect(character.profile).toEqual("hacker");
+    expect(character.profile).toEqual("Hacker");
     expect(character.inventory).toEqual([]);
-    expect(character.stats).toEqual([]);
+    expect(character.stats).toEqual(undefined);
     expect(character.experience).toEqual(0);
   });
-});
 
-describe('StatList', () => {
-
-  test('should assign stats to stat keys', () => {
-    const thugGuy = new StatList(100, 100, 150, 50);
-    expect(thugGuy.health).toEqual(100);
-    expect(thugGuy.defense).toEqual(100);
-    expect(thugGuy.strength).toEqual(150);
-    expect(thugGuy.speed).toEqual(false);
+  test('should assign an object to character stats', () => {
+    character.addStatList();
+    expect(character.stats).toEqual({health: 100, defense: 100, strength: 100, speed: 100});
+    expect(character.stats.health).toEqual(100);
   });
 });
+
+// describe('StatList', () => {
+
+//   test('should assign stats to stat keys', () => {
+//     const thugGuy = new StatList(100, 100, 150, 50);
+//     expect(thugGuy.health).toEqual(100);
+//     expect(thugGuy.defense).toEqual(100);
+//     expect(thugGuy.strength).toEqual(150);
+//     expect(thugGuy.speed).toEqual(50);
+//   });
+// });
+
+// describe('CharacterStart', () => {
+
+//   test('should push profile stats to character stats', () => {
+//     const character = new CharacterCreator("Jason", "hacker");
+//     const hackerTest = new StatList();
+//     character.setStartingStats();
+//     expect(hackerTest.health).toEqual(100);
+//     expect(hackerTest.defense).toEqual(100);
+//     expect(hackerTest.strength).toEqual(100);
+//     expect(hackerTest.speed).toEqual(100);
+//     expect(character.stats).toEqual(100, 100, 100, false);
+//   });
+// });
+
--- src/RPG.js
+++ src/RPG.js
@@ -1,22 +1,45 @@
-let statlist = new StatList;
+let statList = new StatList;
 let inventoryEquip = new InventoryEquip;
 let characterCreator = new CharacterCreator;
 
+// character creation
 export function CharacterCreator(name, profile, inventory, stats, experience) {
   this.name = name;
   this.profile = profile;
   this.inventory = [];
-  this.stats = [];
+  this.stats = stats;
   this.experience = 0;
 }
 
+// CharacterCreator.prototype.addProfile = function(type) {
+//   if(this.profile === thug) {
+//     this.profile = "Thug";
+//   } else if (this.profile === hacker) {
+//     this.profile = "Hacker";
+//   } else if (this.profile === gunSlinger) {
+//     this.profile = "Gun Slinger";
+//   }
+// }
+
+CharacterCreator.prototype.addStatList = function(statValue) {
+  if (this.profile === "Hacker") {
+    let hacker = new StatList(100, 100, 100, 100);
+    this.stats = hacker
+  }
+  // let thug = (100, 100, 150, 50)
+  // let hacker = (100, 100, 100, 100)
+  // let gunSlinger = (100, 50, 100, 150)
+  // this.stats.push(statList)
+}
+
+// inventory 
 function InventoryEquip(armor, weapon, trinket, consumables) {
   this.armor = armor;
   this.weapon = weapon;
   this.trinket = trinket;
   this.consumables = consumables;
 }
-
+// stat list
 export function StatList(health, defense, strength, speed) {
   this.health = health;
   this.defense = defense;
@@ -28,11 +51,11 @@ StatList.prototype.CharacterStart = function(stats) {
   let thug = (100, 100, 150, 50)
   let hacker = (100, 100, 100, 100)
   let gunSlinger = (100, 50, 100, 150)
-  if (this.profile === thug) {
-    statList.push(thug);
-  } else if (this.profile === hacker) {
-    statList.push(hacker);
-  } else if (this.profile === gunSlinger) {
-    statList.push(gunSlinger);
+  if (characterCreator.profile === "thug") {
+    statlist.push(thug);
+  } else if (characterCreator.profile === "hacker") {
+    statlist.push(hacker);
+  } else if (characterCreator.profile === "gunSlinger") {
+    statlist.push(gunSlinger);
   } 
 }

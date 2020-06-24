95c7810b0446685022303f8b77cf9cc5ec8ae7ef
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-24 11:13:35 -0700

    red green refactor test for success for statlist
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- __tests__/RPG.tests.js
+++ __tests__/RPG.tests.js
@@ -1,5 +1,5 @@
 import { CharacterCreator } from './../src/RPG.js';
-// import { StatList } from './../src/RPG.js';
+import { StatList } from './../src/RPG.js';
 
 describe('CharacterCreator', () => {
 
@@ -10,5 +10,16 @@ describe('CharacterCreator', () => {
     expect(character.inventory).toEqual([]);
     expect(character.stats).toEqual([]);
     expect(character.experience).toEqual(0);
+  });
+});
+
+describe('StatList', () => {
+
+  test('should assign stats to stat keys', () => {
+    const thugGuy = new StatList(100, 100, 150, 50);
+    expect(thugGuy.health).toEqual(100);
+    expect(thugGuy.defense).toEqual(100);
+    expect(thugGuy.strength).toEqual(150);
+    expect(thugGuy.speed).toEqual(false);
   });
 });

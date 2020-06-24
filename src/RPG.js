961a44a2edb12f91fba851924735315aae20b390
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-24 10:22:42 -0700

    add statlist prototype character start
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- src/RPG.js
+++ src/RPG.js
@@ -1,3 +1,7 @@
+let statlist = new StatList;
+let inventoryEquip = new InventoryEquip;
+let characterCreator = new CharacterCreator;
+
 function CharacterCreator(name, profile, inventory, stats, experience) {
   this.name = name;
   this.profile = profile;
@@ -18,4 +22,17 @@ function StatList(health, defense, strength, speed) {
   this.defense = defense;
   this.strength = strength;
   this.speed = speed;
+}
+
+StatList.prototype.CharacterStart = function(stats) {
+  let thug = (100, 100, 150, 50)
+  let hacker = (100, 100, 100, 100)
+  let gunSlinger = (100, 50, 100, 150)
+  if (this.profile === thug) {
+    StatList.push(thug)
+  } else if (this.profile === hacker) {
+    StatList.push(hacker)
+  } else (this.profile === gunSlinger) {
+    StatList.push(gunSlinger)
+  } 
 }
